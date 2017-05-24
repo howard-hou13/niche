28963345f48d52b5113db218d348b2ced22c859d
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2017-05-24 17:43:39 -0400

    Add TabBarIOS Adding an empty class used to create the tab bars for the app.

---
+++ src/Niche.js
@@ -0,0 +1,14 @@
+/**
+ * This file contains the main logic for the app.
+ * Programmers: Howard Hou, Richard Zeng
+ */
+
+import React, {Component} from 'react';
+import {
+
+} from 'react-native';
+import Style from './NicheStyle';
+
+export default class Niche extends Component{
+
+}
---
+++ src/NicheStyle.js
@@ -0,0 +1,13 @@
+/**
+ * This file contains the styling for Niche.js file.
+ * Programmers: Howard Hou, Richard Zeng
+ */
+
+ import {StyleSheet} from 'react-native';
+
+ var Style = StyleSheet.create({
+
+ });
+
+
+ export default Style;
