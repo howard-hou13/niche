c795d452ad21d0f04865649225a592180b37d88d
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2017-06-11 17:28:06 -0400

    Change Database Schema Now use name of notification as name of database.

--- src/NotiModel.js
+++ src/NotiModel.js
@@ -5,7 +5,6 @@ class Noti {}
 Noti.schema = {
     name: 'Noti',
     properties: {
-        title: {type: 'string', optional: false},
         message: {type: 'string', optional: true},
         date: 'date'
     }
